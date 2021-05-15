3d6a8900bdb7e45e3fd0b3265cc6843ae9a3f97d
Saha12sra <74057039+Saha12sra@users.noreply.github.com>
2021-05-15 16:32:34 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -39,3 +39,102 @@ function new_image(get_image){
     });
     
     }
+window.addEventListener("keydown",myKeyDown);
+
+function myKeyDown(e){
+console.log(e);
+key_pressed=e.keyCode;
+console.log(key_pressed);
+
+if(key_pressed=='84'){
+    new_image("trunk.jpg");
+    console.log('t');
+}
+
+if(key_pressed=='68'){
+    new_image("dark_green.png");
+    console.log('d');
+}
+
+if(key_pressed=='76'){
+    new_image("light_green.png");
+    console.log('l');
+}
+
+if(key_pressed=='71'){
+    new_image("ground.png");
+    console.log('g');
+}
+
+if(key_pressed=='84'){
+    new_image("trunk.jpg");
+    console.log('t');
+}
+
+if(key_pressed=='87'){
+    new_image("wall.jpg");
+    console.log('w');
+}
+
+if(key_pressed=='89'){
+    new_image("yellow_wall.png");
+    console.log('y');
+}
+
+if(key_pressed=='67'){
+    new_image("cloud.jpg");
+    console.log('c');
+}
+
+if(key_pressed=='82'){
+    new_image("roof.jpg");
+    console.log('r');
+}
+
+if(key_pressed=='85'){
+    new_image("unique.png");
+    console.log('u');
+}
+
+if(e.shiftKey==true && key_pressed=='80'){
+
+    block_width= block_width +10;
+    block_height=block_height+10;
+    document.getElementById("current_width").innerHTML=block_width;
+    document.getElementById("current_height").innerHTML=block_height;
+   
+}
+
+if(e.shiftKey==true && key_pressed=='77'){
+
+    block_width= block_width -10;
+    block_height=block_height-10;
+    document.getElementById("current_width").innerHTML=block_width;
+    document.getElementById("current_height").innerHTML=block_height;
+   
+}
+
+if(key_pressed=='38'){
+
+    up();
+    console.log("up");
+}
+
+if(key_pressed=='40'){
+
+    down();
+    console.log("down");
+}
+
+if(key_pressed=='37'){
+
+    left();
+    console.log("left");
+}
+
+if(key_pressed=='39'){
+
+    right();
+    console.log("right");
+}
+}
